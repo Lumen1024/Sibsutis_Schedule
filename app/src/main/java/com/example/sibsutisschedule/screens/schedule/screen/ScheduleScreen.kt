d3675c4774e6f51248701946e68f0a321ddba956
Lumen <123202753+Lumen1024@users.noreply.github.com>
2023-10-11 17:43:50 +0700

    Optimize imports

--- app/src/main/java/com/example/sibsutisschedule/screens/schedule/screen/ScheduleScreen.kt
+++ app/src/main/java/com/example/sibsutisschedule/screens/schedule/screen/ScheduleScreen.kt
@@ -1,28 +1,14 @@
 package com.example.sibsutisschedule.screens.schedule.screen
 
-import androidx.compose.animation.Crossfade
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.sibsutisschedule.screens.schedule.ScheduleEvent
 import com.example.sibsutisschedule.screens.schedule.ScheduleState
 
